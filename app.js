f188a1e9a969b10ffbd7f04f3a6e0f1da6a20835
P <paula.deville@posteo.net>
2022-09-05 11:01:58 +0200

    mobile design OK

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const burger = document.getElementById("burger");
-const navMobile = document.getElementById("navmobile");
+const navMobile = document.getElementById("nav-mobile");
 const body = document.querySelector("body");
 const header = document.querySelector("header");
+const navLinks = document.querySelectorAll("#nav-mobile ul li a");
 burger.addEventListener('click', () => {
     burger.classList.toggle('menu-active');
     navMobile.classList.toggle('menu-active');
     body.classList.toggle('menu-active');
     header.classList.toggle('menu-active');
+    for (let navLink of navLinks) {
+        navLink.classList.toggle("menu-active");
+    }
+
 })
