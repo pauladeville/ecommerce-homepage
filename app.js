50f017302eea2b5b88725905d9a0f28ca22d5f2c
P <paula.deville@posteo.net>
2022-09-05 12:20:43 +0200

    slideshow init

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+/*NAV*/
 const burger = document.getElementById("burger");
+const burgerBars = document.querySelectorAll("#burger span");
 const navMobile = document.getElementById("nav-mobile");
 const body = document.querySelector("body");
-const header = document.querySelector("header");
+const logo = document.querySelector("header a");
 const navLinks = document.querySelectorAll("#nav-mobile ul li a");
-burger.addEventListener('click', () => {
-    burger.classList.toggle('menu-active');
-    navMobile.classList.toggle('menu-active');
-    body.classList.toggle('menu-active');
-    header.classList.toggle('menu-active');
-    for (let navLink of navLinks) {
-        navLink.classList.toggle("menu-active");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("menu-active");
+  navMobile.classList.toggle("menu-active");
+  body.classList.toggle("menu-active");
+  for (let navLink of navLinks) {
+    navLink.classList.toggle("menu-active");
+  }
+});
+window.onscroll = function () {
+  if (window.scrollY >= 100) {
+    for (let burgerBar of burgerBars) {
+      burgerBar.style.backgroundColor = "transparent";
     }
+    logo.style.color = "transparent";
+  }
+  if (window.scrollY < 100) {
+    for (let burgerBar of burgerBars) {
+      burgerBar.style.backgroundColor = "#fff";
+      logo.style.color = "#fff";
 
-})
+    }
+  }
+};
